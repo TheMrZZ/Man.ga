ee3f96722c9dbdd4c9b1a158d083bfdbb2b59a50
Florian ERNST <florianernst59@gmail.com>
2018-12-10 18:04:01 -0500

    Added a way to add comments

--- php/manga-information.php
+++ php/manga-information.php
@@ -1,42 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8"/>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Man.ga - One Piece</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous"/>
-  <link href="../css/main.css" rel="stylesheet" type="text/css">
-  <link href="../css/manga-information.css" rel="stylesheet" type="text/css">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="Florian Ernst">
-  <meta name="description" content="Man.ga manga information - get the latest information on the manga One Piece.">
-  <meta name="keywords" content="Man.ga,manga,mangas,latest,information,one,piece,One Piece">
-</head>
-
-<body>
-<header>
-  <?php
-  require './navbar.php';
-  echo $navbar;
-  ?>
-</header>
-
 <?php
 require "./connect.php";
 
-$id = $_GET['id'];
+session_start();
+
+$id = 0;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $id = $_POST['id'];
+}
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  $id = $_GET['id'];
+}
+
 $statement = $conn->prepare('SELECT id, name, author, typeID, status, description FROM manga WHERE id = :id');
 $statement->bindParam(':id', $id);
 
 $statement->execute();
 $manga = $statement->fetch();
-
+/*
 if (empty($manga)) {
   header('Location: 404.html');
 }
+*/
 
 $type = ucwords($conn->query('SELECT name FROM type WHERE id = ' . $manga['typeID'])->fetch()['name']);
 
@@ -64,7 +48,59 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 $comArray = $conn->query('SELECT comment, userID FROM comment WHERE mangaID = ' . $manga['id'])->fetchall();
 
 $imageID = $manga['id'];
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//Comment form
+  if ($_POST['form'] == 'send-comment') {
+    $stmt = $conn->prepare("INSERT INTO comment (userID, mangaID, comment) VALUES (:usid, :manid , :com)");
+
+    $usid = $_SESSION['id'];
+    $manid = $id;
+    $com = $_POST['content'];
+
+    $stmt->bindParam(':usid', $_SESSION['id']);
+    $stmt->bindParam(':manid', $id);
+    $stmt->bindParam(':com', $com);
+
+    if ($stmt->execute()) {
+      header("location: manga-information.php?id=" . $manga['id']);
+    } else {
+      echo "Something went wrong. Please try again later.<br>";
+      echo "Error " . $stmt->errorCode() . ":<br>";
+      var_dump($stmt->errorInfo());
+      exit();
+    }
+  }
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Man.ga - One Piece</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous"/>
+  <link href="../css/main.css" rel="stylesheet" type="text/css">
+  <link href="../css/manga-information.css" rel="stylesheet" type="text/css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Florian Ernst">
+  <meta name="description" content="Man.ga manga information - get the latest information on the manga One Piece.">
+  <meta name="keywords" content="Man.ga,manga,mangas,latest,information,one,piece,One Piece">
+</head>
+
+<body>
+<header>
+  <?php
+  require './navbar.php';
+  echo $navbar;
+  ?>
+</header>
 
 <main>
   <header>
@@ -146,6 +182,30 @@ $imageID = $manga['id'];
 
     ?>
 
+  </section>
+  <section id=put_comment>
+    <form class="sign" method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+      <h2 class="login">Let your comment</h2>
+      <!--
+      <div>
+        <p class="label-txt"><label for="subject">RATING</label></p>
+        <p>
+          <label>
+            <input class="min-rating" name="rating" type="number" min="0" max="5" value="0">
+          </label> stars
+        </p>
+      </div>
+    -->
+      <div>
+        <p class="label-txt"><label for="content">ENTER YOUR CONTENT</label></p>
+        <textarea name="content" id="content" rows="15" cols="10" placeholder="Write here" required></textarea><br/>
+      </div>
+      <!--suppress HtmlFormInputWithoutLabel -->
+      <input type="number" value="<?php echo $manga['id']; ?>" name="id" hidden>
+      <!--suppress HtmlFormInputWithoutLabel -->
+      <input type="text" name="form" value="send-comment" hidden>
+      <button type="submit" id="submit" class="submit">Send</button>
+    </form>
   </section>
 </main>
 
